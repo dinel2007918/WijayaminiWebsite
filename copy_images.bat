0a6b1729bb561f952b79333250b952fa8eae2647
Dinel Kithsura <dinel2007918@gmail.com>
2026-07-09 18:40:29 +0530

    add the site map

--- copy_images.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-copy /Y "C:\Users\user\.gemini\antigravity\brain\60324f21-2695-41b1-a7a3-88bd5c6d63cc\home_img_1_1783178618227.png" "c:\Users\user\Desktop\New folder\website\image\home img 1.png"
-copy /Y "C:\Users\user\.gemini\antigravity\brain\60324f21-2695-41b1-a7a3-88bd5c6d63cc\home_img_2_1783178648615.png" "c:\Users\user\Desktop\New folder\website\image\home img 2.png"
-copy /Y "C:\Users\user\.gemini\antigravity\brain\60324f21-2695-41b1-a7a3-88bd5c6d63cc\home_img_3_1783178666646.png" "c:\Users\user\Desktop\New folder\website\image\home img 3.jpeg"
-echo New calm-vibe images copied successfully!
-pause
